d9ae778e1f3fb5357b1f6c6a275a6acb6aaa8822
kumarc <kumarc@opentext.com>
2024-04-28 01:35:02 +0530

    First commit

---
+++ src/test/java/com/SSAssignment/onboardify/cloudconfigservice/CloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSAssignment.onboardify.cloudconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
